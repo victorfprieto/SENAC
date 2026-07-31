81fbd4ab22bfccdca9af60777c49d16039400355
victorfprieto <victorfp.tech@gmail.com>
2026-07-31 11:33:55 -0300

    update notas_script.js
    Ajuste na indentação

--- 2. JavaScript/Estruturas Condicionais/01 Notas/notas_script.js
+++ 2. JavaScript/Estruturas Condicionais/01 Notas/notas_script.js
@@ -1,10 +1,10 @@
 /* Exercício 01:*/
-        let nota1 = Number.parseFloat(prompt("Digite a primeira nota"));
-        let nota2 = Number.parseFloat(prompt("Digite a segunda nota"));
+    let nota1 = Number.parseFloat(prompt("Digite a primeira nota"));
+    let nota2 = Number.parseFloat(prompt("Digite a segunda nota"));
 
-        let soma = nota1 + nota2;
-        if (soma < 60){
-            alert('Nota Final '+ soma +'.\n\nReprovado!');
-        } else {
-            alert('Nota Final '+ soma +'.\n\nAprovado!');
-        }
+    let soma = nota1 + nota2;
+    if (soma < 60){
+        alert('Nota Final '+ soma +'.\n\nReprovado!');
+    } else {
+        alert('Nota Final '+ soma +'.\n\nAprovado!');
+    }

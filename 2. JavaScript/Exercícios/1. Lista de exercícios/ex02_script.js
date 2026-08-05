9be7837ee0eb3487999c42d7e713fe6b13de6577
victorfprieto <victorfp.tech@gmail.com>
2026-08-05 09:25:06 -0300

    exercício 02 - ajustes
    correção do bug de quando digita letras invés de números ao pedir as notas

--- 2. JavaScript/Exercícios/1. Lista de exercícios/ex02_script.js
+++ 2. JavaScript/Exercícios/1. Lista de exercícios/ex02_script.js
@@ -1,19 +1,19 @@
 /*Exercício 02*/
 let nota;
 let soma = 0;
-
+ 
 for (let i = 1; i <= 4; i++) {
-    do {
-        nota = Number(prompt("Digite a nota do "+ i +" bimestre (0 a 10): "));
-
-        if (nota < 0 || nota > 10) {
-            alert("Nota inválida!");
-        }
-    } while (nota < 0 || nota > 10);
-
-    soma = soma + nota;
+  do {
+    nota = Number(prompt("Digite a nota do " + i + "º bimestre (0 a 10): "));
+ 
+    if (isNaN(nota) || nota < 0 || nota > 10) {
+      alert("Nota inválida! Digite apenas números entre 0 e 10.");
+    }
+  } while (isNaN(nota) || nota < 0 || nota > 10);
+ 
+  soma = soma + nota;
 }
-
+ 
 let media = soma / 4;
 
-alert("A média do aluno é: "+ media);
+alert("A média do aluno é: " + media);

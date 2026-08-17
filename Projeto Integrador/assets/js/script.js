8b5f806aae64f3889a8c6dae62b7fa2a4456bd5e
Victor Prieto <victorfp.tech@gmail.com>
2026-08-17 09:03:29 -0300

    adicionando JS
    adicionando Menu Hamburger no projeto

---
+++ Projeto Integrador/assets/js/script.js
@@ -0,0 +1,31 @@
+/* DevHook - Script Principal
+
+    No momento, as principais funções do site são:
+    - Menu Hamburger;
+
+*/
+
+// MENU HAMBURGER
+const botaoMenu = document.getElementById("botao-menu");
+const navegacao = document.getElementById("navegacao-principal");
+ 
+if (botaoMenu && navegacao) {
+ 
+    botaoMenu.addEventListener("click", function () {
+ 
+        navegacao.classList.toggle("menu-aberto");
+        botaoMenu.classList.toggle("menu-aberto");
+ 
+        const menuAberto = navegacao.classList.contains("menu-aberto");
+ 
+        botaoMenu.setAttribute("aria-expanded", menuAberto);
+ 
+        if (menuAberto) {
+            botaoMenu.setAttribute("aria-label", "Fechar menu");
+        } else {
+            botaoMenu.setAttribute("aria-label", "Abrir menu");
+        }
+ 
+    });
+ 
+}

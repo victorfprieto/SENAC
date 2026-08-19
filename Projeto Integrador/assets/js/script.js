5cc37f98e182f02da4080bc4d4b8f1e18576e776
Victor Prieto <victorfp.tech@gmail.com>
2026-08-19 11:44:02 -0300

    update script.js

--- Projeto Integrador/assets/js/script.js
+++ Projeto Integrador/assets/js/script.js
@@ -11,26 +11,26 @@
 // MENU HAMBURGER
 var botaoMenu = document.getElementById("botao-menu");
 var navegacao = document.getElementById("navegacao-principal");
- 
+
 if (botaoMenu && navegacao) {
- 
+
     botaoMenu.addEventListener("click", function () {
- 
+
         navegacao.classList.toggle("menu-aberto");
         botaoMenu.classList.toggle("menu-aberto");
- 
+
         const menuAberto = navegacao.classList.contains("menu-aberto");
- 
+
         botaoMenu.setAttribute("aria-expanded", menuAberto);
- 
+
         if (menuAberto) {
             botaoMenu.setAttribute("aria-label", "Fechar menu");
         } else {
             botaoMenu.setAttribute("aria-label", "Abrir menu");
         }
- 
+
     });
- 
+
 }
 
 /* FILTROS DE VAGAS */
